e0944dfe565f6e56ab752a95b2a2f6f1fca8cf7f
willchrisjr <willchrisjr301@gmail.com>
2024-06-03 03:49:52 -0400

    Create a blob object

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-
+# My Git Implementation
 
 In this challenge, you'll build a small Git implementation that's capable of
 initializing a repository, creating commits and cloning a public repository.
 Along the way we'll learn about the `.git` directory, Git objects (blobs,
 commits, trees etc.), Git's transfer protocols and more.
 
-**Note**: If you're viewing this repo on GitHub, head over to
-[codecrafters.io](https://codecrafters.io) to try the challenge.
+
 
 # Passing the first stage
 
@@ -55,7 +54,6 @@ alias mygit=/path/to/your/repo/your_git.sh
 mkdir -p /tmp/testing && cd /tmp/testing
 mygit init
 ```
-# My Git Implementation
 
 ## Commands
 
--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 import sys
 import os
 import zlib
+import hashlib
 
 def main():
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -16,28 +15,49 @@ def main():
     elif command == "cat-file" and sys.argv[2] == "-p":
         blob_sha = sys.argv[3]
         read_blob(blob_sha)
+    elif command == "hash-object" and sys.argv[2] == "-w":
+        file_path = sys.argv[3]
+        hash_object(file_path)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
 def read_blob(blob_sha):
-    # The first two characters of the SHA-1 hash are the directory name
     dir_name = blob_sha[:2]
-    # The remaining 38 characters are the file name
     file_name = blob_sha[2:]
-    # Construct the path to the object file
     object_path = os.path.join(".git", "objects", dir_name, file_name)
     
-    # Read and decompress the object file
     with open(object_path, "rb") as f:
         compressed_data = f.read()
         decompressed_data = zlib.decompress(compressed_data)
     
-    # Extract the content from the decompressed data
     null_byte_index = decompressed_data.index(b'\x00')
     content = decompressed_data[null_byte_index + 1:]
     
-    # Print the content to stdout without a newline at the end
     sys.stdout.buffer.write(content)
+
+def hash_object(file_path):
+    with open(file_path, "rb") as f:
+        content = f.read()
+    
+    size = len(content)
+    header = f"blob {size}\0".encode()
+    store = header + content
+    
+    sha1 = hashlib.sha1(store).hexdigest()
+    
+    dir_name = sha1[:2]
+    file_name = sha1[2:]
+    object_path = os.path.join(".git", "objects", dir_name, file_name)
+    
+    if not os.path.exists(os.path.join(".git", "objects", dir_name)):
+        os.mkdir(os.path.join(".git", "objects", dir_name))
+    
+    compressed_data = zlib.compress(store)
+    
+    with open(object_path, "wb") as f:
+        f.write(compressed_data)
+    
+    print(sha1)
 
 if __name__ == "__main__":
     main()
